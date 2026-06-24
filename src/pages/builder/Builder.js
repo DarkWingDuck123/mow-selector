07c0a8c26286effcbcc28ba46d8a2efdb707bed2
Drake Mallard <drakemllrd@gmail.com>
2026-06-24 17:13:04 -0600

    Reset the open fleet when switching rulesets
    selectedListId was local component state with no tie to the active
    ruleset, so switching rulesets in the Builder left a fleet from the
    old ruleset open in the editor instead of clearing the selection.

--- src/pages/builder/Builder.js
+++ src/pages/builder/Builder.js
@@ -42,6 +42,10 @@ export const Builder = () => {
   }, [location.pathname]);
 
   useEffect(() => {
+    setSelectedListId(null);
+  }, [selectedRuleset]);
+
+  useEffect(() => {
     game &&
     !factions &&
     fetcher({
